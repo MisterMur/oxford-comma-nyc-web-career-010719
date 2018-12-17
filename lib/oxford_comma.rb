585f72b2ba91ca4a56e5a1bd08fbf45cdeb30863
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-17 20:19:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
-  array.insert(-2,"and")
-  array.join(", ")
-  array.delete(array.index("and")+1)
+  
+  if array.length==1
+    return array[0]
+  elsif array.length ==2
+    array.insert(1,"and")
+    return array.join(' ')
+  elsif array.length >=3
+    array.insert(-2,"and")
+    array.join(", ")
+    array.delete(array.index("and")+1)
+    return array
+  end
 end
